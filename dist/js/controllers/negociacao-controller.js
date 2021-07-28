e137d40f3623a162a6057fbea9efbf293678583a
Doughert Fernandes <doughertvf@gmail.com>
2021-07-27 23:56:30 -0300

    first commit

---
+++ dist/js/controllers/negociacao-controller.js
@@ -0,0 +1,18 @@
+import { Negociacao } from "../models/negociacao.js";
+export class NegociacaoController {
+    constructor() {
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+    }
+    adiciona() {
+        const negociacao = new Negociacao(new Date(this.inputData.value.replace(/-/g, ",")), parseInt(this.inputQuantidade.value), parseFloat(this.inputValor.value));
+        this.limparFormulario();
+    }
+    limparFormulario() {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
+    }
+}
---
+++ dist/js/models/negociacao.js
@@ -0,0 +1,19 @@
+export class Negociacao {
+    constructor(data, quatidade, valor) {
+        this._data = data;
+        this._quantidade = quatidade;
+        this._valor = valor;
+    }
+    get data() {
+        return this._data;
+    }
+    get quantidade() {
+        return this._quantidade;
+    }
+    get valor() {
+        return this._valor;
+    }
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+}
